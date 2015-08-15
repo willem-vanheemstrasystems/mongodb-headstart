e746b5cfb8d0c55f104afbc25db4b125dc859ed3
Willem van Heemstra <wvanheemstra@icloud.com>
2015-08-15 11:31:37 +0100

    Modified

--- databases/db.js
+++ databases/db.js
@@ -2,17 +2,42 @@ var mongodb = require('mongodb'),
 	dbServer = new mongodb.Server('localhost', 27017, {auto_reconnect:true}),
 	db = new mongodb.Db('mydb', dbServer, {w:1});
 
+/*	
+// open database connection
 db.open( function(err, conn){
-	// add a collcetion to the db
-	db.collection('myCollection', function(err, collection){
-		// insert a document into the collection
-		collection.insert({
-				a: 'my item'	
-			}, function(err,result){
-			// log the result
+	// select and or add a collection to the db
+	db.collection('myNewCollection', function(err, collection){
+		// cache a count variable
+		var count = 0;
+		// insert numbers into the collection
+		for(var i=0; i<5; i++) {
+			collection.insert({
+				num: i
+			}, function(err, result) {
+			  // log the result
+			  console.log(result);
+			  // increment the count value
+			  count++;
+			  // if the count is high enough, close the connection
+			  if (count > 4) {
+				  db.close();
+			  }
+			});
+		}
+	});
+});
+*/
+
+// open database connection
+db.open(function(err, conn){
+	// select the collection
+	db.collection('myNewCollection', function(err, collection) {
+		// select all the documents in the collection
+		collection.find().toArray(function(err, result) {
+			// log the data
 			console.log(result);
 			// close the connection
-			db.close();		
+			db.close();
 		});
 	});
 });
