20f97dafcc618a95a28edfaef8709abd101e5b10
Willem van Heemstra <wvanheemstra@icloud.com>
2015-08-15 11:42:11 +0100

    Modified

--- databases/db.js
+++ databases/db.js
@@ -2,7 +2,7 @@ var mongodb = require('mongodb'),
 	dbServer = new mongodb.Server('localhost', 27017, {auto_reconnect:true}),
 	db = new mongodb.Db('mydb', dbServer, {w:1});
 
-/*	
+/* INSERT 5 DOCUMENTS 
 // open database connection
 db.open( function(err, conn){
 	// select and or add a collection to the db
@@ -28,12 +28,29 @@ db.open( function(err, conn){
 });
 */
 
+/* FIND ALL DOCUMENTS
 // open database connection
 db.open(function(err, conn){
 	// select the collection
 	db.collection('myNewCollection', function(err, collection) {
 		// select all the documents in the collection
 		collection.find().toArray(function(err, result) {
+			// log the data
+			console.log(result);
+			// close the connection
+			db.close();
+		});
+	});
+});
+*/
+
+/* FIND ALL DOCUMENT WITH num = 2 */
+// open database connection
+db.open(function(err, conn){
+	// select the collection
+	db.collection('myNewCollection', function(err, collection) {
+		// select all the documents where num is 2 in the collection
+		collection.find({num: 2}).toArray(function(err, result) {
 			// log the data
 			console.log(result);
 			// close the connection
