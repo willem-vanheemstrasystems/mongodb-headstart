a6779fc87e057198301f838e7d52a79a13164077
Willem van Heemstra <wvanheemstra@icloud.com>
2015-08-15 10:49:23 +0100

    Added

---
+++ databases/db.js
@@ -0,0 +1,18 @@
+var mongodb = require('mongodb'),
+	dbServer = new mongodb.Server('localhost', 27017, {auto_reconnect:true}),
+	db = new mongodb.Db('mydb', dbServer, {w:1});
+
+db.open( function(err, conn){
+	// add a collcetion to the db
+	db.collection('myCollection', function(err, collection){
+		// insert a document into the collection
+		collection.insert({
+				a: 'my item'	
+			}, function(err,result){
+			// log the result
+			console.log(result);
+			// close the connection
+			db.close();		
+		});
+	});
+});
